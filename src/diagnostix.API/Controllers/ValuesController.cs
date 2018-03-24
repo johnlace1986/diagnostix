6f441816b8cc607f7e348a0ddc5988f8a7e4951f
John Lace <lace.john@gmail.com>
2018-03-24 00:53:40 +0000

    Renamed incorrectly named API project.

--- src/diagnostix.API/Controllers/ValuesController.cs
+++ src/diagnostix.API/Controllers/ValuesController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace diagnsotix.API.Controllers
+namespace diagnostix.API.Controllers
 {
     [Route("api/[controller]")]
     [Authorize]
--- src/diagnostix.API/Program.cs
+++ src/diagnostix.API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace diagnsotix.API
+namespace diagnostix.API
 {
     public class Program
     {
