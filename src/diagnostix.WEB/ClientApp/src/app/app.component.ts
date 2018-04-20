269ca91cb4d574bcb52bd84b35bb15a91a4de39d
John Lace <lace.john@gmail.com>
2018-04-20 13:48:38 +0100

    Added WEB project that authenticates against 3rd party identity server.

---
+++ src/diagnostix.WEB/ClientApp/src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+
+import { OAuthService } from 'angular-oauth2-oidc';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+
+  constructor(private oAuthService: OAuthService) {
+
+    // URL of the SPA to redirect the user to after login
+    this.oAuthService.redirectUri = "https://localhost:44377/";
+    this.oAuthService.silentRefreshRedirectUri = "https://localhost:44377/";
+    this.oAuthService.postLogoutRedirectUri = "https://localhost:44377/";
+
+    // The SPA's id. The SPA is registerd with this id at the auth-server
+    this.oAuthService.clientId = "diagnostix.WEB";
+
+    // set the scope for the permissions the client should request
+    // The first three are defined by OIDC. The 4th is a usecase-specific one
+    this.oAuthService.scope = "openid profile roles email";
+    this.oAuthService.responseType = "id_token token";
+    this.oAuthService.silentRefreshTimeout = 5000;
+
+    // set to true, to receive also an id_token via OpenId Connect (OIDC) in addition to the
+    // OAuth2-based access_token
+    this.oAuthService.oidc = true;
+
+    // Use setStorage to use sessionStorage or another implementation of the TS-type Storage
+    // instead of localStorage
+    this.oAuthService.setStorage(sessionStorage);
+
+    // The name of the auth-server that has to be mentioned within the token
+    this.oAuthService.issuer = "https://sso.local.galadirectory.co.uk/identity";
+
+    // Load Discovery Document and then try to login the user
+    this.oAuthService.loadDiscoveryDocument().then(() => {
+
+      // This method just tries to parse the token(s) within the url when
+      // the auth-server redirects the user back to the web-app
+      // It dosn't send the user the the login page
+      this.oAuthService.tryLogin({});
+
+    });
+
+  }
+}
---
+++ src/diagnostix.WEB/ClientApp/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {
+
+  constructor(private oAuthService: OAuthService) {
+  }
+
+  public login() {
+    this.oAuthService.initImplicitFlow();
+  }
+
+  public logoff() {
+    this.oAuthService.logOut();
+  }
+
+  public get name() {
+    let claims = this.oAuthService.getIdentityClaims();
+    if (!claims) return null;
+    return claims.name;
+  }
+}
